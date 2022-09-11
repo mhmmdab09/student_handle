15f37999b9b22f1cc43a8e055e2b2c80cea39ecc
Mohammad Abtahi <smabtahi922gmail.com>
2022-09-11 15:22:31 +0430

    structure build

--- app.py
+++ app.py
@@ -6,3 +6,25 @@ app = Flask(__name__)
 def main_page():
     '''main page'''
     return("hello")
+
+
+@app.route('/v1/sigup_student')
+def signup_student():
+    pass
+
+
+@app.route('/v1/renew_student')
+def renew_student():
+    pass
+
+
+@app.route('/v1/use_session')
+def use_session():
+    pass
+
+
+@app.route('/v1/list_students')
+def list_students():
+    pass
+
+
